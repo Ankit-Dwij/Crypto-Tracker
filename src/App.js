060e36d36edfd9a823fc44b1df92fb03095ca470
Ankit Dwij <ankitdwiz010@gmail.com>
2022-04-26 21:11:27 +0530

    api call success

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Coin from "./coin";
+
+function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    async function fetch() {
+      const response =
+        await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false
+    `);
+      setCoins(response.data);
+    }
+    fetch();
+  }, []);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <>
+      <div className="coin-app">
+        <div className="coin-search">
+          <h1 className="coin-text">Search a currency</h1>
+          <form>
+            <input
+              type="text"
+              className="coin-input"
+              placeholder="search"
+              onChange={handleChange}
+            />
+          </form>
+          {filteredCoins.map((coin) => {
+            return (
+              <Coin
+                key={coin.id}
+                image={coin.image}
+                name={coin.name}
+                symbol={coin.symbol}
+                price={coin.current_price}
+                volume={coin.market_cap}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
