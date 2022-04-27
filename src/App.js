1740e7bdf8946ecb66b0d4d6c814bd7b4e10b558
Ankit Dwij <ankitdwiz010@gmail.com>
2022-04-27 12:34:53 +0530

    added coin.css & app.css

--- src/App.js
+++ src/App.js
@@ -45,7 +45,9 @@ function App() {
                 name={coin.name}
                 symbol={coin.symbol}
                 price={coin.current_price}
-                volume={coin.market_cap}
+                marketCap={coin.market_cap}
+                priceChange={coin.price_change_percentage_24h}
+                volume={coin.total_volume}
               />
             );
           })}
--- src/coin.jsx
+++ src/coin.jsx
@@ -1,17 +1,34 @@
 import React from "react";
+import "./coin.css";
 
-const Coin = ({ image, name, symbol, price, volume }) => {
+const Coin = ({
+  image,
+  name,
+  symbol,
+  price,
+  volume,
+  priceChange,
+  marketCap,
+}) => {
   return (
     <div className="coin-container">
       <div className="coin-row">
         <div className="coin">
           <img src={image} alt="image" />
           <h1>{name}</h1>
-          <p className="coin-symbol">{symbol}</p>
+          <p className="coin-symbol">{symbol.toUpperCase()}</p>
         </div>
-        <div className="coin data">
-          <p className="coin-price">{price}</p>
-          <p className="coin-volume">{volume.toLocaleString()}</p>
+        <div className="coin-data">
+          <p className="coin-price">₹ {price.toLocaleString()}</p>
+          <p className="coin-volume">₹ {volume.toLocaleString()}</p>
+          {priceChange < 0 ? (
+            <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+          ) : (
+            <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
+          )}
+          <p className="coin-marketcap">
+            Mkt Cap : ₹{marketCap.toLocaleString()}
+          </p>
         </div>
       </div>
     </div>
